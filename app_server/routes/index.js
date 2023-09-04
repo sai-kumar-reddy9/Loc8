f3ff28a31bf1f9e902eefa262f59a37cabca7e45
coder049 <saikumarreddy049@gmail.com>
2023-09-04 11:27:50 +0530

    1st commit

---
+++ app_server/routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const ctrlLocations =
+require('../controllers/locations');
+const ctrlOthers = require('../controllers/others');
+/* Locations pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+/* Other pages */
+router.get('/about', ctrlOthers.about);
+module.exports = router;
